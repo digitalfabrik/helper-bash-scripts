3dd6b63080c62c2c51b10c3d7e67ff68aa55b36f
Sven Seeberg <sseebergelverfeldt@suse.com>
2018-03-04 15:43:42 +0100

    make removable for other users

--- backup_database.sh
+++ backup_database.sh
@@ -6,6 +6,7 @@ bzip2 ~/database.sql
 openssl rand -base64 32 > ~/key.bin
 openssl enc -aes-256-cbc -salt -in ~/database.sql.bz2 -out /tmp/ig-backup/database-$CURRENT_DATE.sql.bz2.enc -pass file:~/key.bin
 openssl rsautl -encrypt -inkey ~/backup_pubkey.pem -pubin -in ~/key.bin -out /tmp/ig-backup/key-$CURRENT_DATE.bin.enc
+chmod -R 777 /tmp/ig-backup
 rm ~/key.bin
 cd /tmp/ig-backup
 find ./ -mtime +1 -type f -delete
