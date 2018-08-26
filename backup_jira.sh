0b99ffa4bebb7ac27fad204687c620805415d708
Sven Seeberg <sven@geeq.de>
2018-08-26 11:47:28 +0200

    Fix jira backup

--- backup_jira.sh
+++ backup_jira.sh
@@ -4,14 +4,14 @@ mkdir /tmp/jira-backup ~/jira/
 mysqldump -u root jiradb > ~/database.sql
 rsync -a --exclude /var/atlassian/application-data/jira/plugins --exclude /var/atlassian/application-data/jira/data/git-plugin /var/atlassian/application-data/jira/* ~/jira/
 tar -cf ~/backup.tar ~/database.sql ~/jira/
-bzip2 ~/database.tar
+bzip2 ~/backup.tar
 openssl rand -base64 32 > ~/key.bin
-openssl enc -aes-256-cbc -salt -in ~/database.tar.bz2 -out /tmp/jira-backup/database-$CURRENT_DATE.tar.bz2.enc -pass file:$HOME/key.bin
+openssl enc -aes-256-cbc -salt -in ~/backup.tar.bz2 -out /tmp/jira-backup/backup-$CURRENT_DATE.tar.bz2.enc -pass file:$HOME/key.bin
 openssl rsautl -encrypt -inkey ~/backup_pubkey.pem -pubin -in ~/key.bin -out /tmp/jira-backup/key-$CURRENT_DATE.bin.enc
 chmod -R 777 /tmp/jira-backup
 rm ~/key.bin
 rm ~/database.sql
-rm ~/database.tar.bz2
+rm ~/backup.tar.bz2
 rm -rf ~/jira/
 cd /tmp/jira-backup
 find ./ -mtime +1 -type f -delete
