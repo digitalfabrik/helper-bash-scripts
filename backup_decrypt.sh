8cf36e6c52315abfd8d3678a5092856bbb333dad
Sven Seeberg <sven@geeq.de>
2018-09-22 15:10:24 +0200

    Specify md5 as decryption digest for 1.0.2/1.1.0 compatibility

--- backup_decrypt.sh
+++ backup_decrypt.sh
@@ -4,4 +4,5 @@ if [[ -z $1 || $1 == "--help" || $1 == "-h" ]]; then
 	exit
 fi
 openssl rsautl -decrypt -inkey $1 -in $2 -out ~/key.bin
-openssl enc -d -aes-256-cbc -in $3 -out ~/database.sql.bz2 -pass file:$HOME/key.bin
+# For compatibility between versions 1.0.2 and 1.1. specify MD5 as the digest
+openssl enc -d -md MD5 -aes-256-cbc -in $3 -out ~/database.sql.bz2 -pass file:$HOME/key.bin
