b9ef5bfdd527165e6b49ab99a4aa7a8851f5ba30
Sven Seeberg <mail@sven-seeberg.de>
2019-02-28 14:27:54 +0100

    Fix -md MD5 parmeter for decrypting backup

--- backup/backup_decrypt.sh
+++ backup/backup_decrypt.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 if [[ -z $1 || $1 == "--help" || $1 == "-h" ]]; then
 	echo "Usage: decrypt_database.sh [PRIVKEY] [AES_KEY] [DATABASE]"
+	echo "In some cases you might have to add the '-md MD5' parameter to the decryption command of the database SQL file.
 	exit
 fi
 openssl rsautl -decrypt -inkey $1 -in $2 -out ~/key.bin
 # For compatibility between versions 1.0.2 and 1.1. specify MD5 as the digest
-openssl enc -d -md MD5 -aes-256-cbc -in $3 -out ~/database.sql.bz2 -pass file:$HOME/key.bin
+openssl enc -d -aes-256-cbc -in $3 -out ~/database.sql.bz2 -pass file:$HOME/key.bin
