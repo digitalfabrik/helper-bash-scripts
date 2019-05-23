84a53d52c2dcd9170fc26b64067cc57fce1036b8
Sven Seeberg <mail@sven-seeberg.de>
2019-05-23 18:47:30 +0200

    Get ph text and tail from seg

--- tmx-tools/split-tmx.py
+++ tmx-tools/split-tmx.py
@@ -2,18 +2,41 @@
 
 import sys
 import os
-from xml.etree import ElementTree, cElementTree
-
+#from xml.etree import ElementTree, cElementTree
+from lxml import etree
+import copy
 
 def generate_tmx(args):
-	old_tree = ElementTree.parse(os.path.join(os.getcwd(), args.input))
+	old_tree = etree.parse(os.path.join(os.getcwd(), args.input))
+	old_body = old_tree.xpath('/tmx/body')
 
+	new_tree = copy.deepcopy(old_tree)
+	new_body = new_tree.xpath('/tmx/body')
 
-	new_root = cElementTree.Element("tmx", version="1.4")
+	for old_tu in old_tree.xpath('/tmx/body/tu'):
+		segments = get_segments(old_tu)
+		#new_tu = etree.SubElement(body, changedate=old_tu.attrib['changedate'])
 
 	outfile = os.path.join(os.getcwd(), args.output)
 	new_tree = cElementTree.ElementTree(new_root)
 	new_tree.write(outfile)
+
+def get_segments(old_tu):
+	bpt = True if old_tu.xpath(".//tuv/seg/bpt") else False
+
+	if not bpt:
+		return False
+	new_translation = {}
+	for old_tuv in old_tu.findall(".//tuv"):
+		lang = old_tuv.attrib['{http://www.w3.org/XML/1998/namespace}lang']
+		print ("")
+		print ("--- NEW TUV ---")
+		print ("")
+		seg = old_tuv.findall(".//seg")[0]
+		print("Segment text: {}".format(seg.text))
+		for ph in seg.findall(".//ph"):
+			print("PH text: {}".format(ph.text))
+			print("PH tail: {}".format(ph.tail))
 
 def parse_cli(cliargs=None):
 	import argparse
