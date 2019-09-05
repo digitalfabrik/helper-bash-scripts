304000f0d487eb438b9cc4df6a649dad87b11e5d
Sven Seeberg <mail@sven-seeberg.de>
2019-09-05 09:02:38 +0200

    Update backup scripts
    * Use variables, include scp'ing backups to another
      server.

--- backup/backup_database.sh
+++ backup/backup_database.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+TARGET=""
+BACKUPDIR="/tmp/ig-backup"
 CURRENT_DATE=$(date +%Y%m%d-%H%M)
-mkdir /tmp/ig-backup
+mkdir $BACKUPDIR
 mysqldump -u root ig_cms > ~/database.sql
 bzip2 ~/database.sql
 openssl rand -base64 32 > ~/key.bin
-openssl enc -aes-256-cbc -salt -in ~/database.sql.bz2 -out /tmp/ig-backup/database-$CURRENT_DATE.sql.bz2.enc -pass file:$HOME/key.bin
-openssl rsautl -encrypt -inkey ~/backup_pubkey.pem -pubin -in ~/key.bin -out /tmp/ig-backup/key-$CURRENT_DATE.bin.enc
-chmod -R 777 /tmp/ig-backup
+openssl enc -aes-256-cbc -salt -in ~/database.sql.bz2 -out $BACKUPDIR/database-$CURRENT_DATE.sql.bz2.enc -pass file:$HOME/key.bin
+openssl rsautl -encrypt -inkey ~/backup_pubkey.pem -pubin -in ~/key.bin -out $BACKUPDIR/key-$CURRENT_DATE.bin.enc
+chmod -R 700 /tmp/ig-backup
 rm ~/key.bin
 rm ~/database.sql.bz2
-cd /tmp/ig-backup
-find ./ -mtime +1 -type f -delete
-
+if [ -n "$TARGET" ]; then
+  scp $BACKUPDIR/key-$CURRENT_DATE.bin.enc $TARGET
+  scp $BACKUPDIR/database-$CURRENT_DATE.sql.bz2.enc $TARGET
+fi
+find $BACKUPDIR -mtime +7 -type f -delete
--- backup/backup_jira.sh
+++ backup/backup_jira.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
+BACKUPDIR="/tmp/jira-backup"
+TARGET=""
 CURRENT_DATE=$(date +%Y%m%d-%H%M)
-mkdir /tmp/jira-backup ~/jira/
+mkdir $BACKUPDIR ~/jira/
 mysqldump -u root jiradb > ~/database.sql
 rsync -a --exclude /var/atlassian/application-data/jira/plugins --exclude /var/atlassian/application-data/jira/data/git-plugin /var/atlassian/application-data/jira/* ~/jira/
 tar -cf ~/backup.tar ~/database.sql ~/jira/
 bzip2 ~/backup.tar
 openssl rand -base64 32 > ~/key.bin
-openssl enc -aes-256-cbc -salt -in ~/backup.tar.bz2 -out /tmp/jira-backup/backup-$CURRENT_DATE.tar.bz2.enc -pass file:$HOME/key.bin
-openssl rsautl -encrypt -inkey ~/backup_pubkey.pem -pubin -in ~/key.bin -out /tmp/jira-backup/key-$CURRENT_DATE.bin.enc
-chmod -R 777 /tmp/jira-backup
+openssl enc -aes-256-cbc -salt -in ~/backup.tar.bz2 -out $BACKUPDIR/backup-$CURRENT_DATE.tar.bz2.enc -pass file:$HOME/key.bin
+openssl rsautl -encrypt -inkey ~/backup_pubkey.pem -pubin -in ~/key.bin -out $BACKUPDIR/key-$CURRENT_DATE.bin.enc
+chmod -R 700 $BACKUPDIR
 rm ~/key.bin
 rm ~/database.sql
 rm ~/backup.tar.bz2
 rm -rf ~/jira/
-find /tmp/jira-backup/ -mtime +1 -type f -delete
+if [ -n "$TARGET" ]; then
+  scp $BACKUPDIR/key-$CURRENT_DATE.bin.enc $TARGET
+  scp $BACKUPDIR/backup-$CURRENT_DATE.sql.bz2.enc $TARGET
+fi
+find $BACKUPDIR/ -mtime +7 -type f -delete
 
