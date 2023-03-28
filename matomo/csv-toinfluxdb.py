ca77f7ab76dff159274b0b67c0891d275f6a82da
Sven Seeberg <mail@sven-seeberg.de>
2023-03-28 18:16:24 +0200

    Fix timestamp

--- matomo/csv-toinfluxdb.py
+++ matomo/csv-toinfluxdb.py
@@ -5,7 +5,7 @@ Send data from statistics CSV to InfluxDb
 
 import argparse
 import csv
-import time
+import datetime
 import requests
 
 
@@ -17,11 +17,13 @@ def period_string():
         return "day"
     return "month"
 
-def log_influxdb(key, value, period, timestamp=time.time_ns()):
+def log_influxdb(period, key, value, timestamp):
     """
     Log data to InfluxDB & Grafana
     """
-    influx_data = "cms-access,period={} {}={} {}".format(period, key, value, timestamp)
+    influx_data = "cms-access,period={} {}={} {}".format(
+        period, key, value,
+        int(datetime.datetime.strptime(timestamp, "%Y-%m-%d").strftime('%s'))*1000*1000)
     requests.post('https://monitoring.tuerantuer.org/write?db=okr',
                   influx_data.encode(),
                   cert=('/etc/pki/client.crt', '/etc/pki/client.key'),
@@ -37,8 +39,9 @@ def main():
             for key in ["offline-downloads", "total-downloads"]:
                 log_influxdb(period_string(), key, row[key], row["date"])
 
-
 PARSER = argparse.ArgumentParser()
 PARSER.add_argument("--csv", help="Path to statistics CSV file")
 PARSER.add_argument("--daily", help="Plot daily not monthly intervals.", action='store_true')
 ARGS = PARSER.parse_args()
+
+main()
